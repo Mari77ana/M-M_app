b70651866c09d0f4e3edfb177e47f2c8404fddb5
masamarojevic <marojevicmasa@gmail.com>
2023-10-19 10:54:18 +0200

    commit on main view adding note in grid
    Created Journal class , Journal entry struct, working on adding a note and putting it in our lazy grid

---
+++ M_M_App _exam/M_M_App/Views/AddNotePhoto.swift
@@ -0,0 +1,20 @@
+//
+//  AddNotePhoto.swift
+//  M_M_App
+//
+//  Created by Ana Marojevic on 2023-10-19.
+//
+
+import SwiftUI
+
+struct AddNotePhoto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddNotePhoto_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNotePhoto()
+    }
+}

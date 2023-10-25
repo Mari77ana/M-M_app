174d368324d43310e7cf2c008a7cbcbc53073877
Mariana Laic <m.laic@hotmail.com>
2023-10-25 13:46:10 +0200

    added Test2

---
+++ M_M_App _exam/M_M_App/Views/UnAuthenticated/Test2.swift
@@ -0,0 +1,18 @@
+//
+//  Test2.swift
+//  M_M_App
+//
+//  Created by Mariana Laic on 2023-10-25.
+//
+
+import SwiftUI
+
+struct Test2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test2()
+}

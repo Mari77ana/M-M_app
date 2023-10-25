eff731cbefaeec7096e17fa25ef15e287e2bb5fc
masamarojevic <marojevicmasa@gmail.com>
2023-10-25 13:43:08 +0200

    Added new test view

---
+++ M_M_App _exam/M_M_App/Views/UnAuthenticated/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  M_M_App
+//
+//  Created by Ana Marojevic on 2023-10-25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}

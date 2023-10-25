5d5e8b5004f17adbb25203eeb9a9e17c31c40354
Mariana Laic <m.laic@hotmail.com>
2023-10-25 14:50:11 +0200

    Added SpalshScreenView_File

---
+++ M_M_App _exam/M_M_App/Views/UnAuthenticated/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  M_M_App
+//
+//  Created by Mariana Laic on 2023-10-25.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}

ae67dfdad341b4f9f7036e918e9225f2acd4c004
Mariana Laic <m.laic@hotmail.com>
2023-11-08 10:56:52 +0100

    ApiCall to Advice
    gjorde en request på Apiet Advice, responce slip.advice

--- M_M_App _exam/M_M_App/Authenticated/API/ApiModel/ApiCall.swift
+++ M_M_App _exam/M_M_App/Authenticated/API/ApiModel/ApiCall.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+
+class ApiCall {
+    
+    /// Api call request, returns response advice
+    func fetchAdvice(endpoint: String) async throws -> String {
+        
+        guard let url = URL(string: endpoint) else{throw ApiErrors.invalidURL}
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let decoder = JSONDecoder()
+        let adviceResponse = try decoder.decode(AdviceResponse.self, from: data)
+        
+        return adviceResponse.slip.advice
+    }
+    
+    
+    
+    
+    
+
+    
+    
+    ///  Errors Feedback
+    enum ApiErrors: Error {
+        
+        case invalidURL
+        case invaldResponse
+        case invalidData
+    }
+    
+    
+    
+    
+}

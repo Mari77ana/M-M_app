6e546a0d122a98614f480770c234cc3e1f84c7a8
masamarojevic <marojevicmasa@gmail.com>
2023-11-12 19:11:12 +0100

    fixin splash, check if it works on mariannas

--- M_M_App _exam/M_M_App/Views/UnAuthenticated/SplashScreenView.swift
+++ M_M_App _exam/M_M_App/Views/UnAuthenticated/SplashScreenView.swift
@@ -6,67 +6,121 @@
 //
 
 import SwiftUI
-
+//
+//struct SplashScreenView: View {
+//    
+//    @State var rotationAngle: Double = 0.0
+//    @State var size: Double = 0.1
+//    @State var opacity: Double = 0.6
+//    @State var isActive = false
+//    
+//    @State var currentUser: UserData? //= UserData(firstname: "", lastname: "")
+//    
+//    
+//    var body: some View {
+//        
+//        if isActive {
+//           
+//            /// if currentUser != nil
+//            if currentUser?.id != nil {
+//                /// Navigate to MainView
+//                MainView()
+//            }else{
+//                /// Else to LoginView
+//                LoginView(db: DbConnection())
+//            }
+//           
+//           
+//        }else {
+//            
+//            VStack{
+//                Image("cameraIcon")
+//                    .resizable()
+//                    .frame(width: 60, height: 60)
+//                    .rotationEffect(.degrees(rotationAngle))
+//                    .scaleEffect(size)
+//                    .opacity(opacity)
+//                    .onAppear{
+//                        withAnimation(Animation.linear(duration: 2)
+//                            . repeatCount(2)){
+//                                rotationAngle = 365.0
+//                                self.size = 2.5
+//                                self.opacity = 1.0
+//                            }
+//                    }
+//            }
+//            .onAppear{
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+//                    isActive = true
+//                }
+//            }
+//            
+//        }
+//     
+//        
+//        
+//    }
+//}
+//
+//
+//struct SplashScreenView_Previews: PreviewProvider {
+//    
+//    static var previews: some View {
+//       SplashScreenView()
+//    }
+//    
+//}
+//
 struct SplashScreenView: View {
+    @StateObject var db = DbConnection()
+    @EnvironmentObject var themeColor: ThemeColor
     
-    @State var rotationAngle: Double = 0.0
-    @State var size: Double = 0.1
-    @State var opacity: Double = 0.6
-    @State var isActive = false
-    
-    @State var currentUser: UserData? //= UserData(firstname: "", lastname: "")
-    
-    
+    @State private var isActive = false
+    @State private var rotationAngle: Double = 0.0
+    @State private var size: Double = 0.1
+    @State private var opacity: Double = 0.6
+
     var body: some View {
-        
-        if isActive {
-           
-            /// if currentUser != nil
-            if currentUser?.id != nil {
-                /// Navigate to MainView
-                MainView()
-            }else{
-                /// Else to LoginView
-                LoginView(db: DbConnection())
-            }
-           
-           
-        }else {
-            
-            VStack{
-                Image("cameraIcon")
-                    .resizable()
-                    .frame(width: 60, height: 60)
-                    .rotationEffect(.degrees(rotationAngle))
-                    .scaleEffect(size)
-                    .opacity(opacity)
-                    .onAppear{
-                        withAnimation(Animation.linear(duration: 2)
-                            . repeatCount(2)){
-                                rotationAngle = 365.0
-                                self.size = 2.5
-                                self.opacity = 1.0
-                            }
-                    }
-            }
-            .onAppear{
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
-                    isActive = true
-                }
-            }
-            
+        VStack {
+            Image("cameraIcon")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .rotationEffect(.degrees(rotationAngle))
+                .scaleEffect(size)
+                .opacity(opacity)
         }
-     
-        
-        
+        .onAppear {
+            triggerSplashAnimation()
+        }
+        .onChange(of: db.currentUser) { _ in
+            triggerSplashAnimation()
+        }
+        .fullScreenCover(isPresented: $isActive) {
+            if db.currentUser != nil {
+                MainView().environmentObject(db).environmentObject(themeColor)
+            } else {
+                LoginView(db: db).environmentObject(db)
+            }
+        }
     }
-}
 
-
-struct SplashScreenView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-       SplashScreenView()
+    private func triggerSplashAnimation() {
+        // Reset animation properties
+        rotationAngle = 0.0
+        size = 0.1
+        opacity = 0.6
+        isActive = false
+        
+        // Start the animation
+        withAnimation(Animation.linear(duration: 2).repeatCount(1, autoreverses: false)) {
+            rotationAngle = 365.0
+            size = 2.5
+            opacity = 1.0
+        }
+        
+        // Delay the state change to trigger the navigation
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+            isActive = true
+        }
     }
-    
 }

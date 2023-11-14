d2eef28563017cdae207deaab57ab927d4bc23c1
masamarojevic <marojevicmasa@gmail.com>
2023-11-14 20:06:50 +0100

    deleted files and cleaned from comments

--- M_M_App _exam/M_M_App/Model/Grid.swift
+++ M_M_App _exam/M_M_App/Model/Grid.swift
@@ -8,54 +8,9 @@
 import Foundation
 import SwiftUI
 
-//struct Grid: View {
-//
-//    @ObservedObject var noteClass: NoteClass
-//
-//    let columns: [GridItem] = [
-//
-//        GridItem(.fixed(50), spacing: 30, alignment: nil),
-//        GridItem(.fixed(50), spacing: 30, alignment: nil),
-//        GridItem(.fixed(50), spacing: 30, alignment: nil)
-//
-//    ]
-//    var body: some View{
-//
-//
-//        ScrollView{
-//            LazyVGrid(columns: columns,
-//                      alignment: .center,
-//                      spacing: 15,
-//                      pinnedViews: [],
-//                      content: {
-//                ForEach(noteClass.getNotes()){
-//                entry in
-//                    if let imageURL = entry.imageURL, let url = URL(string: imageURL){
-//                        NavigationLink(destination: ViewNotePhoto(entry: entry, journal: NoteClass)){
-//                            AsyncImage(url: url){ image in
-//                                image.resizable()
-//                            } placeholder: {
-//                                ProgressView()
-//                            }
-//                            .frame(width: 70, height: 80)
-//                            .cornerRadius(8)
-//                        }
-//                    } else{
-//                        NavigationLink(destination: ViewNotePhoto(entry: entry, journal: NoteClass)){
-//                            Rectangle()
-//                                .fill(Color.blue)
-//                                .frame(width: 70, height: 80)
-//                        }
-//                    }
-//
-//
-//
-//        }
-//    }
-//                      }}}
-//
+
 struct Grid: View {
-    //@ObservedObject var noteClass: NoteClass
+    
     @ObservedObject var noteVM: NotesViewModel
     
     let columns: [GridItem] = [
@@ -65,27 +20,27 @@ struct Grid: View {
     ]
 
     var body: some View {
+        
         ScrollView {
+            
             LazyVGrid(columns: columns, alignment: .center, spacing: 10) {
+                
                 ForEach(noteVM.notes) { entry in
+                    
                     if let imageURL = entry.imageURL, let url = URL(string: imageURL) {
-                        // Use AsyncImage to load and display the image
+                        
+                        // AsyncImage to load and display the image
                         NavigationLink(destination: ViewNotePhoto(entry: entry, noteVM: noteVM)) {
+                            
                             AsyncImage(url: url) { image in
+                                
                                 image.resizable()
+                                
                             } placeholder: {
                                 ProgressView()
                             }
                             .frame(width: 120, height: 120)
                             .cornerRadius(8)
-                        }
-                    } else {
-                        // If there is no image, show a placeholder
-                        NavigationLink(destination: ViewNotePhoto(entry: entry, noteVM: noteVM)) {
-                            Rectangle()
-                                .fill(Color.gray)
-                                .frame(width: 120, height: 120)
-                                .cornerRadius(8)
                         }
                     }
                 }
